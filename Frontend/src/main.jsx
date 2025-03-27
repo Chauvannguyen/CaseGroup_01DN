eabb92e176b4cb9d12b08d110428f7d7718ac559
van nguyen <nguyencvpd10794@gmail.com>
2025-03-27 21:15:13 +0700

    nguyenup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import BookList from "./components/BookList.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<BookList />} />
+          </Routes>
+      </BrowserRouter>
+  </StrictMode>
 )
