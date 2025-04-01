a84b6331d4df4a061082407a74f7fa2d6bc1bcf3
vuntde170750 <169011916+vuntde170750@users.noreply.github.com>
2025-04-01 18:51:34 +0700

    Vũ commit Home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {Route, Routes} from "react-router";
 import BookList from "./Pages/BookList.jsx";
 import Login from "./Pages/Login.jsx";
+import Home from "./Pages/Home.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Routes>
               <Route path="/Login" element={<Login />}/>
               <Route path="/" element={<BookList />} />
+              <Route path="/Home" element={<Home />} />
           </Routes>
       </>
   )
---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Footer from "../components/Footer/footer.jsx";
+import Header from "../components/Header/header.jsx";
+import {Button, Card} from "react-bootstrap";
+
+const Home = () => {
+    const [books, setBooks] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3000/books')
+            .then((response) => {
+                setBooks(response.data);
+            })
+            .catch((error) => {
+                if (error.code === 'ERR_NETWORK') {
+                    setError('No Data');
+                }
+            });
+    }, []);
+
+    return (
+        <div className="container py-4">
+            <Header/>
+            <h2>The Book Haven</h2>
+            <div className="row">
+                {books.map((book) => (
+                    <div className="col-md-3 mb-4" key={book.id}>
+                        <Card>
+                            <Card.Img variant="top" src={book.image} style={{height: '300px', objectFit: 'cover'}} />
+                            <Card.Body>
+                                <Card.Title>{book.title}</Card.Title>
+                                <Card.Text>Tác giả: {book.author}</Card.Text>
+                                <Button variant="primary">Xem chi tiết</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                ))}
+            </div>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
